4dd477525edfd0b19e7b3714e52f836c330d22a3
Ardhiyatma Raditya Wibawa <ardiyatma.wibawa12@gmail.com>
2020-07-29 13:22:05 +0700

    Third modul - menguji personal token laravel passport

--- app/Tweet.php
+++ app/Tweet.php
@@ -13,4 +13,9 @@ class Tweet extends Model
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
+
+    public function scopeLatestFirst($query)
+    {
+        return $query->orderBy('id','asc');
+    }
 }
